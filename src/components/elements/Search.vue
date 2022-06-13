29b60814a7229d8cf59f64fbec42be5fcde465a1
elahe karamzade <eli.ka1991@gmail.com>
2022-06-13 19:06:04 +0430

    optimize search functionallity by blur on search input

--- src/components/elements/Search.vue
+++ src/components/elements/Search.vue
@@ -5,6 +5,7 @@
       name="search"
       id="search"
       v-model="search"
+      @blur="searchKey"
       class="shadow-sm focus:ring-red-500 focus:border-red-500 block w-full sm:text-sm border-gray-300 py-2 px-4 rounded-full bg-gray-100"
       placeholder="saerch..."
     />
@@ -18,15 +19,22 @@ export default {
       search: null,
     };
   },
-  watch: {
-    search(val) {
-      if (val)
-        this.$store.dispatch("getPeople", {
-          search: val,
-        });
-      else {
-        this.$store.dispatch("getPeople");
-      }
+  // watch: {
+  //   search(val) {
+  //     if (val && val.length > 3)
+  //       this.$store.dispatch("getPeople", {
+  //         search: val,
+  //       });
+  //     else {
+  //       this.$store.dispatch("getPeople");
+  //     }
+  //   },
+  // },
+  methods: {
+    searchKey() {
+      this.$store.dispatch("getPeople", {
+        search: this.search,
+      });
     },
   },
 };
